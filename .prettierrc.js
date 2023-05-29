1b74ab836e7088b1ed793c17b5b4e18bfd343322
heesankim <opqryt45@gmail.com>
2023-05-30 03:31:23 +0900

    Feat : 서버 폴더구조 및 환경세팅

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always"
+};
