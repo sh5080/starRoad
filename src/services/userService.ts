75e53a647cf2369002fee76c18bc303dad3b4e0d
heesankim <opqryt45@gmail.com>
2023-06-01 23:07:41 +0900

    -Fix:에러처리 방법 수정

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,12 @@
+// DTO : 어떤형식으로 데이터가 가야하는지
+// ENTITY : db에서 가져온값. 1 ROW 1 ENTITY
+
 import bcrypt from 'bcrypt';
 import { createUser, getUserById } from '../models/user';
 import jwt from 'jsonwebtoken';
 import config from '../config';
 import { UserType } from '../types/user';
+import { AppError } from '../api/middlewares/errorHandler';
 
 const { saltRounds } = config.bcrypt;
 const ACCESS_TOKEN_SECRET = config.jwt.ACCESS_TOKEN_SECRET;
@@ -13,9 +17,9 @@ const REFRESH_TOKEN_EXPIRES_IN = config.jwt.ACCESS_TOKEN_EXPIRES_IN;
 export const signupUser = async (user: UserType): Promise<string> => {
   const hashedPassword = await bcrypt.hash(user.password, saltRounds);
 
-  const findUserid = await getUserById(user.userId);
-  if (findUserid) {
-    throw new Error('이미 사용중인 아이디입니다.');
+  const findUserId = await getUserById(user.userId);
+  if (findUserId) {
+    throw new AppError('이미 존재하는 아이디입니다.', 409);
   }
 
   await createUser({ ...user, password: hashedPassword });
@@ -25,14 +29,13 @@ export const signupUser = async (user: UserType): Promise<string> => {
 
 export const loginUser = async (userId: string, password: string): Promise<object> => {
   const user = await getUserById(userId);
-  //console.log(user)
+
   if (!user) {
-    throw new Error('존재하지 않는 아이디입니다.');
+    throw new AppError('없는 사용자 입니다.', 404);
   }
   const isPasswordMatch = await bcrypt.compare(password, user.password);
-
   if (!isPasswordMatch) {
-    throw new Error('비밀번호가 일치하지 않습니다.');
+    throw new AppError('비밀번호가 일치하지 않습니다.', 401);
   }
 
   const accessToken: string = jwt.sign({ userId: user.userId }, ACCESS_TOKEN_SECRET, {
@@ -46,13 +49,11 @@ export const loginUser = async (userId: string, password: string): Promise<objec
   return { accessToken, refreshToken };
 };
 
-
-
 export const getUser = async (userId: string) => {
   const user = await getUserById(userId);
-  //console.log(user)
+
   if (!user) {
-    throw new Error('없는 사용자 입니다.');
+    throw new AppError('없는 사용자 입니다.', 404);
   }
   const { password, ...userData } = user;
 
