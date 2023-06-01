9dffdbcc389310966b071951f95b1b27bcdbcf9e
heesankim <opqryt45@gmail.com>
2023-06-01 23:05:51 +0900

    -Fix : 에러 핸들러 수정

--- src/api/middlewares/errorHandler.ts
+++ src/api/middlewares/errorHandler.ts
@@ -1,11 +1,27 @@
 import { Request, Response, NextFunction } from 'express';
 
-const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.status || 500).send({
-    error: {
-      message: err.message,
-    },
-  });
+class AppError extends Error {
+  status: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.status = status;
+  }
+}
+
+const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    res.status(err.status).send({
+      error: {
+        message: err.message,
+      },
+    });
+  } else {
+    res.status(500).send({
+      error: {
+        message: 'Unexpected error occurred',
+      },
+    });
+  }
 };
 
-export default errorHandler;
+export { errorHandler, AppError };
