31023040ef63a8b8ff176c837f8bdb6106c99ef4
heesankim <opqryt45@gmail.com>
2023-06-01 23:07:11 +0900

    -Fix: 쿼리문 코드 테이블명 변경

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,7 +5,7 @@ export const createUser = async (user: UserType): Promise<void> => {
   const pool = db;
   const connection = await pool.getConnection();
   try {
-    await connection.execute('INSERT INTO users (name, userId, password, email) VALUES (?, ?, ?, ?)', [
+    await connection.execute('INSERT INTO User (name, userId, password, email) VALUES (?, ?, ?, ?)', [
       user.name,
       user.userId,
       user.password,
@@ -21,7 +21,7 @@ export const getUserById = async (userId: string): Promise<UserType | null> => {
   const pool = await db;
   const connection = await pool.getConnection();
   try {
-    const [rows] = await connection.execute('SELECT * FROM users WHERE userId = ?', [userId]);
+    const [rows] = await connection.execute('SELECT * FROM User WHERE userId = ?', [userId]);
     if (Array.isArray(rows) && rows.length > 0) {
       const userData = rows[0] as UserType;
       return userData;
