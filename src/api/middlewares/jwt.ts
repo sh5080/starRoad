fbfb8502aabdc74047e10da5b683319a0982f315
heesankim <opqryt45@gmail.com>
2023-06-01 23:06:13 +0900

    -Fix : 에러처리 방법 수정

--- src/api/middlewares/jwt.ts
+++ src/api/middlewares/jwt.ts
@@ -3,6 +3,7 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import config from '../../config/index';
+import { AppError } from '../middlewares/errorHandler';
 
 interface CustomRequest extends Request {
   user?: JwtPayload & { userId: string };
@@ -25,10 +26,11 @@ export const validateToken = async (req: CustomRequest, res: Response, next: Nex
   }
 
   try {
-    // 토큰이 있는데 유효하지 않은 토큰일 경우(만료된 토큰)
     req.user = jwt.verify(accessToken, ACCESS_TOKEN_SECRET) as JwtPayload & { userId: string };
     next();
+    // next(req.user);
   } catch (err: any) {
+    // 토큰이 있는데 유효하지 않은 토큰일 경우(만료된 토큰)
     if (err.name === 'TokenExpiredError') {
       const refreshToken = req.cookies.refreshToken;
 
@@ -43,19 +45,19 @@ export const validateToken = async (req: CustomRequest, res: Response, next: Nex
           expiresIn: ACCESS_TOKEN_EXPIRES_IN,
         });
 
-        res.json({ accessToken: newAccessToken });
+        req.user = { userId: decodedRefreshToken.userId };
+        res.status(200).json({ accessToken: newAccessToken });
         next();
       } catch (err: any) {
         if (err.name === 'TokenExpiredError') {
           res.clearCookie('refreshToken');
-          return res.status(401).json({ message: '리프레쉬 토큰이 만료되었습니다. 다시 로그인 해주세요.' });
+          // return res.status(401).json({ message: '리프레쉬 토큰이 만료되었습니다. 다시 로그인 해주세요.' });
+          next(new AppError('리프레쉬 토큰이 만료되었습니다. 다시 로그인 해주세요.', 401));
         }
-        return res.status(403).json({ message: '토큰이 유효하지 않습니다. 토큰을 확인해주세요.' });
+        next(new AppError('토큰이 유효하지 않습니다. 토큰을 확인해주세요.', 401));
       }
     } else {
-      return res.status(403).json({ message: '토큰이 유효하지 않습니다. 토큰을 확인해주세요.' });
+      next(new AppError('토큰이 유효하지 않습니다. 토큰을 확인해주세요.', 401));
     }
   }
 };
-
-
