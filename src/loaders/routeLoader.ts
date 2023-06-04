c5f63b69ae19763ed61d06d9e57b835ee33d9902
heesankim <opqryt45@gmail.com>
2023-06-04 16:40:00 +0900

    -Feat : adminRouter 추가

--- src/loaders/routeLoader.ts
+++ src/loaders/routeLoader.ts
@@ -2,6 +2,7 @@ import { Application, Request, Response } from 'express';
 import userRouter from '../api/routes/useRoutes';
 import travelRouter from '../api/routes/travelRoutes';
 import diaryRouter from '../api/routes/diaryRoutes';
+import adminRouter from '../api/routes/adminRoutes';
 
 const routeLoader = (app: Application): Application => {
   app.get('/', (req: Request, res: Response) => {
@@ -9,10 +10,9 @@ const routeLoader = (app: Application): Application => {
   });
 
   app.use('/users', userRouter);
-  
   app.use('/travels', travelRouter);
   app.use('/diaries', diaryRouter);
-
+  app.use('/admin', adminRouter);
   return app;
 };
 
