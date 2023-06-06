0d84a81d92f986efb520c5a0c4e127fd9f8613a9
김승환 <prpn97@gmail.com>
2023-06-07 01:05:57 +0900

    Fix: diary 기능 mypage / diary 분배

--- src/api/routes/diaryRoutes.ts
+++ src/api/routes/diaryRoutes.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
 import { 
-    createDiaryController, 
     getAllDiaryController, 
     getOneDiaryController, 
-    updateDiaryController, 
-    deleteDiaryController 
+
 } from '../../controllers/diaryController';
 import { validateToken } from '../middlewares/jwt';
 
 const router = Router();
 
-router.post('/', validateToken, createDiaryController); //여행기 생성
 router.get('/', getAllDiaryController); //여행기 전체 조회
-router.get('/:diary_id', validateToken, getOneDiaryController); //특정 여행기 조회
-router.patch('/', validateToken, updateDiaryController);
-router.delete('/:diary_id', validateToken, deleteDiaryController);
+router.get('/:diary_id', getOneDiaryController); //특정 여행기 조회
+
 
 export default router;
--- src/api/routes/mypageRoutes.ts
+++ src/api/routes/mypageRoutes.ts
@@ -4,16 +4,23 @@ import {
     updateUserInfo, 
     deleteUserInfo
 } from '../../controllers/userController';
-import { getMyDiaryController } from '../../controllers/diaryController';
+import { 
+    createDiaryController,
+    getMyDiaryController,
+    updateDiaryController, 
+    deleteDiaryController
+} from '../../controllers/diaryController';
 
 import { validateToken } from '../middlewares/jwt';
 
 const router = Router();
 
-router.get('/', validateToken, getUserInfo);
-router.patch('/', validateToken, updateUserInfo);
-router.delete('/', validateToken, deleteUserInfo);
+router.get('/user', validateToken, getUserInfo);
+router.patch('/user', validateToken, updateUserInfo);
+router.delete('/user', validateToken, deleteUserInfo);
+
+router.post('/diary', validateToken, createDiaryController); //여행기 생성
 router.get('/diary', validateToken, getMyDiaryController); //나의 여행기 조회
-
-
+router.patch('/diary/:diary_id', validateToken, updateDiaryController);
+router.delete('/diary/:diary_id', validateToken, deleteDiaryController);
 export default router;
