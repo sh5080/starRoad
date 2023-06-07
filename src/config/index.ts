592b281e3f8dfe8dea2bf31b23d2642b0f007fd9
김승환 <prpn97@gmail.com>
2023-06-07 15:49:53 +0900

    Fix: 필수 환경변수 누락시 에러처리 코치님 피드백 반영

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,17 +4,17 @@ import { Config } from '../types/config';
 
 const config: Config = {
   jwt: {
-    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || '',
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || '',
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || (() => { throw new Error('ACCESS_TOKEN_SECRET 환경 변수가 필요합니다.'); })(),
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || (() => { throw new Error('REFRESH_TOKEN_SECRET 환경 변수가 필요합니다.'); })(),
     REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d',
     ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN || '12h',
   },
-  port: process.env.PORT || '',
+  port: process.env.PORT || (() => { throw new Error('PORT 환경 변수가 필요합니다.'); })(),
   database: {
-    DB_HOST: process.env.DB_HOST || '',
-    DB_USER: process.env.DB_USER || '',
-    DB_PASSWORD: process.env.DB_PASSWORD || '',
-    DB_NAME: process.env.DB_NAME || '',
+    DB_HOST: process.env.DB_HOST || (() => { throw new Error('DB_HOST 환경 변수가 필요합니다.'); })(),
+    DB_USER: process.env.DB_USER || (() => { throw new Error('DB_USER 환경 변수가 필요합니다.'); })(),
+    DB_PASSWORD: process.env.DB_PASSWORD || (() => { throw new Error('DB_PASSWORD 환경 변수가 필요합니다.'); })(),
+    DB_NAME: process.env.DB_NAME || (() => { throw new Error('DB_NAME 환경 변수가 필요합니다.'); })(),
   },
   bcrypt: {
     saltRounds: process.env.BCRYPT_SALT_ROUNDS ? Number(process.env.BCRYPT_SALT_ROUNDS) : 10,
