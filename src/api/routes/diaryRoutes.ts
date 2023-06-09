6cea6fc7eb4acccbf4e6442bc93ee21ce28de78d
heesankim <opqryt45@gmail.com>
2023-06-09 17:20:49 +0900

    -Feat: 미들웨어 함수명 수정

--- src/api/routes/diaryRoutes.ts
+++ src/api/routes/diaryRoutes.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', diaryController.getAllDiaries); //여행기 전체 조회
 router.get('/:diary_id', diaryController.getOneDiary); //특정 여행기 조회
 
-router.post('/mypage/diary', validateToken, processImage, diaryController.createDiary); //여행기 작성
+router.post('/mypage/diary', validateToken, processImage, diaryController.createDiaryController); //여행기 작성
 router.get('/mypage/diary', validateToken, diaryController.getMyDiaries); //나의 여행기 조회
 router.patch('/mypage/diary/:diary_id', validateToken, diaryController.updateDiary);
 router.delete('/mypage/diary/:diary_id', validateToken, diaryController.deleteDiary);
