f6069a01b411f0f366096bd58347f7795c11b07e
heesankim <opqryt45@gmail.com>
2023-06-09 17:20:26 +0900

    Feat: 관광지 라우트 추가

--- src/api/routes/adminRoutes.ts
+++ src/api/routes/adminRoutes.ts
@@ -3,7 +3,6 @@ import { validateToken } from '../middlewares/jwt';
 import { ensureAdmin } from '../middlewares/admin';
 import * as adminController from '../../controllers/adminController';
 const router = Router();
-// 관리자 페이지 구현
 
 // [관리자] 모든 회원 조회하기
 router.get('/users', validateToken, ensureAdmin, adminController.getAllUsersController);
@@ -49,5 +48,8 @@ router.delete(
 
 // [관리자] 회원이 작성한 모든 댓글 조회하기
 router.get('/users/:username/comments', validateToken, ensureAdmin, adminController.getUserAllCommentsController);
+
+// [관리자] 관광지 추가하기
+router.post('/locations', validateToken, ensureAdmin, adminController.addTouristDestinationController);
 
 export default router;
