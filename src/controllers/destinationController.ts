f3e0d0d5df3b357308d48c893d5425a7838dc45d
heesankim <opqryt45@gmail.com>
2023-06-10 21:18:45 +0900

    -Feat : 관광지 조회 API 추가

---
+++ src/controllers/destinationController.ts
@@ -0,0 +1,34 @@
+import { CustomRequest } from '../types/customRequest';
+import { Response, NextFunction } from 'express';
+import { AppError, CommonError } from '../types/AppError';
+import * as destinationService from '../services/destinationService';
+
+// 관광지 모두 조회하기
+export const getAllTouristDestinationController = async (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    console.log('모든 여행지 불러오는 중...');
+    const destinations = await destinationService.getAllTouristDestinationService();
+    const destinationCount: number = destinations.length;
+
+    res.status(200).json({ data: { destinations, destinationCount, message: '모든 여행지를 불러왔습니다.' } });
+  } catch (err) {
+    console.error(err);
+    next(new AppError(CommonError.UNEXPECTED_ERROR, '여행지 정보 조회에 실패했습니다.', 500));
+  }
+};
+
+// 관광지 상세 조회하기
+export const getTouristDestinationController = async (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    console.log('관광지 상세 조회중...');
+    const { location_id } = req.params;
+
+    const destination = await destinationService.getTouristDestinationService(Number(location_id));
+    console.log('destination = ', destination);
+
+    res.status(200).json({ data: { destination, message: '여행지 상세 조회를 완료했습니다.' } });
+  } catch (err) {
+    console.error(err);
+    next(new AppError(CommonError.UNEXPECTED_ERROR, '여행지 상세 조회에 실패했습니다.', 500));
+  }
+};
---
+++ src/models/destinationModel.ts
@@ -0,0 +1,32 @@
+import { RowDataPacket } from 'mysql2';
+import { db } from '../loaders/dbLoader';
+import { TouristDestinationType } from '../types/destination';
+import { AppError, CommonError } from '../types/AppError';
+
+// 관광지 전부 조회하기
+export const getAllTouristDestinationModel = async (): Promise<TouristDestinationType[]> => {
+  try {
+    const [rows] = await db.execute('SELECT * FROM travel_destination');
+    const touristDestinations = rows as TouristDestinationType[];
+    console.log(touristDestinations);
+
+    return touristDestinations;
+  } catch (error) {
+    console.error(error);
+    throw new AppError(CommonError.SERVER_ERROR, 'Failed to fetch tourist destinations', 500);
+  }
+};
+
+// 관광지 상세 조회하기
+export const getTouristDestinationModel = async (id: number): Promise<TouristDestinationType> => {
+  try {
+    const [rows] = await db.execute('SELECT * FROM travel_destination WHERE id = ?', [id]);
+    const touristDestination = (rows as RowDataPacket[])[0] as TouristDestinationType;
+    console.log(touristDestination);
+
+    return touristDestination;
+  } catch (error) {
+    console.error(error);
+    throw new AppError(CommonError.SERVER_ERROR, 'Failed to fetch tourist destination', 500);
+  }
+};
