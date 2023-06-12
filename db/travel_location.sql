946fa6cdbc112a78ac26f589243e9b4964d07f51
heesankim <opqryt45@gmail.com>
2023-06-13 01:03:22 +0900

    -Fix : 쿼리문 수정

--- db/travel_location.sql
+++ db/travel_location.sql
@@ -1,14 +1,10 @@
-
 CREATE TABLE `travel_location` (
-    `username` VARCHAR(20) NOT NULL,
     `location_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
     `plan_id` INT UNSIGNED,
     `date` DATE,
-    `location` VARCHAR(100),
-    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `location` VARCHAR(255),
     `order` INT,
-    PRIMARY KEY (`location_id`),
-    FOREIGN KEY (`plan_id`) REFERENCES `travel_plan` (`plan_id`),
-    FOREIGN KEY (`username`) REFERENCES `travel_plan` (`username`)
+    `latitude` DECIMAL(10,8),
+    `longitude` DECIMAL(11,8),
+    PRIMARY KEY (`location_id`)
 );
