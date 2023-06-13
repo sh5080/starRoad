7a5ee0e245e72192a75fa4fbb828cc1786d8f302
heesankim <opqryt45@gmail.com>
2023-06-14 03:33:55 +0900

    Fix : 라우트 코드 순서 수정

--- src/api/routes/diaryRoutes.ts
+++ src/api/routes/diaryRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import * as diaryController from '../../controllers/diaryController';
 
-
 const router = Router();
 
-router.get('/', diaryController.getAllDiaries); //여행기 전체 조회
 router.get('/:diary_id', diaryController.getOneDiary); //특정 여행기 조회
+router.get('/', diaryController.getAllDiaries); //여행기 전체 조회
 
 export default router;
